cd80d9b8f2e2af0593d7853210cb2347d5535cd9
roger.parkinson35 <roger.parkinson35@8bab076f-2c68-7d98-3a50-f0f65909ce3f>
2012-08-02 03:01:31 +0000

    Initial version, though this is all working

---
+++ trunk/Laser.cpp
@@ -0,0 +1,88 @@
+// Do not remove the include below
+#include "Laser.h"
+
+
+/*
+
+
+ Plays a melody on ATTiny85 using tone() PWM pin 4
+
+ circuit:
+ * 8-ohm speaker on digital pin 0
+
+ */
+
+#define TONE_PIN 4
+#define LED_PIN PORTB3
+#define LED_PIN1 PORTB0
+int LED_cycle=0;
+
+
+void setup(void);
+void loop(void);
+
+void setup() {
+  DDRB |= _BV(LED_PIN);
+  DDRB |= _BV(LED_PIN1);
+  PINB |= _BV(LED_PIN);
+  //setupTimer1(myISR);
+
+}
+#ifndef ARDUINO                     //main() function not required in Arduino environment
+int main(void) {
+    setup();
+    for(;;) {loop();}
+    return 0;
+}
+#endif
+void myISR() {                      //this function will be called every 10ms
+    static uint8_t myCounter;
+
+    if (++myCounter == 100) {       //has a second elapsed?
+        myCounter = 0;
+        PINB |= _BV(LED_PIN);           //toggle the LED
+        PINB |= _BV(LED_PIN1);           //toggle the LED
+    }
+}
+
+void loop() {
+  // repeat the melody.
+  static uint8_t myCounter;
+  if (++myCounter == 100) {       //has a second elapsed?
+    myCounter = 0;
+    }
+
+  if (myCounter % 2 == 0) {
+    PINB |= _BV(LED_PIN);           //toggle the LED
+    PINB |= _BV(LED_PIN1);           //toggle the LED
+  }
+
+//  digitalWrite(LED_PIN, HIGH);
+  for (int thisNote=95; thisNote<105; thisNote++)
+  {
+    playNote(thisNote,2);;
+  }
+//  digitalWrite(LED_PIN, LOW);
+//  digitalWrite(LED_PIN1, HIGH);
+
+  for (int thisNote=105; thisNote>95; thisNote--)
+  {
+    playNote(thisNote,2);
+  }
+//  digitalWrite(LED_PIN1, LOW);
+}
+
+void playNote(int note, int duration)
+{
+    int noteDuration = 1000/(duration*4);
+    tone(TONE_PIN, note,noteDuration);
+    int pauseBetweenNotes = noteDuration * 1;//1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(TONE_PIN);
+
+    tone(TONE_PIN, 90,noteDuration);
+    delay(pauseBetweenNotes/4);
+    // stop the tone playing:
+    noTone(TONE_PIN);
+ }
